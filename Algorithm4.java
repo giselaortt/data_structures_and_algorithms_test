1079b8cff76e70828c7325b9dc15508747879270
gisela ortt <ms.giselaortt@gmail.com>
2022-08-26 20:14:12 -0300

    coded the display function and renamed variables

--- Algorithm4.java
+++ Algorithm4.java
@@ -3,6 +3,7 @@
 //
 //Analyses of complexity:
 //
+//
 import java.util.Locale;
 import java.util.ArrayList;
 import java.io.FileReader; 
@@ -10,29 +11,39 @@ import java.util.Scanner;
 
 
 class BackpackSolver{
-    int number_of_objects;
+    int numberOfObjects;
     String[] names;
     int[] weights;
-    ArrayList<Integer> solution;
+    ArrayList< ArrayList <Integer>> solution;
 
     public void BackpackSolver( int[] weights, String[] names, int size ){
         this.names = names;
         this.weights = weights;
-        this.number_of_objects  = size;
+        this.numberOfObjects  = size;
+        this.solution = new ArrayList< ArrayList<Integer> >();
     }
 
-    public void fitObjects(){}
-/*
+
+    //how the subproblems are defined;
+    //how the solutions to these subproblems are defined
+    //how the solutions are calculated in the base case
+    //how the solutions are calculated in the general case
+    public void solve(){}
+
+    
     public static void printSolution(){
-        for(){
-            System.out.printf( "Suitcase: %d\n" );
-            for(){
-                System.out.printf("%s %d\n");
+        for( int i=0; i< this.solution.size(); i++ ){
+            System.out.printf( "Suitcase: %d\n", i );
+            int suiticaseWeight = 0;
+            for( int j=0; j< this.solution.get(i).size(); j++ ){
+                int objectIndex = solution.get(i).get(j);
+                System.out.printf("%s %d\n", names[objectIndex], weights[objectIndex] );
+                suiticaseWeight += weights[objectIndex];
             }
+            System.out.println( "Residual capacity: " + suiticaseWeight );
             System.out.println();
         }
     }
-    */
 }
 
 
@@ -40,22 +51,22 @@ class Algorithm4{
 
     public static void main( String args[] ) throws Exception {
         Locale.setDefault(Locale.US);
-        int number_of_objects;
+        int numberOfObjects;
         FileReader reader = new FileReader( args[0] );
         Scanner scam = new Scanner( reader );
-        number_of_objects = scam.nextInt();
+        numberOfObjects = scam.nextInt();
         scam.nextLine();
-        String[] object_names = new String[number_of_objects];
-        int[] object_weights = new int[number_of_objects];
+        String[] objectsNames = new String[numberOfObjects];
+        int[] objectWeights = new int[numberOfObjects];
         String input_value;
 
-        for( int i=0; i<number_of_objects; i++ ){
+        for( int i=0; i<numberOfObjects; i++ ){
             String[] line = scam.nextLine().trim().replaceAll(" +", " ").split(" ");
-            object_names[i] = line[0];
-            object_weights[i] = Integer.parseInt(line[1]);
-            System.out.printf( "%s,%d\n", object_names[i], object_weights[i] );
+            objectsNames[i] = line[0];
+            objectWeights[i] = Integer.parseInt(line[1]);
+            System.out.printf( "%s,%d\n", objectsNames[i], objectWeights[i] );
         }
         
-        BackpackSolver solver = new BackpackSolver( object_weights, object_names, number_of_objects );
+        BackpackSolver solver = new BackpackSolver( objectWeights, objectsNames, numberOfObjects );
     }
 }
