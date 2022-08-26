6970f1d93fd014e2d5a17ec423bf8b6ffc32a380
gisela ortt <ms.giselaortt@gmail.com>
2022-08-25 21:16:13 -0300

    fixed the input format, it is now correct.

--- Algorithm4.java
+++ Algorithm4.java
@@ -3,21 +3,17 @@
 //
 //Analyses of complexity:
 //
-
-import java.util.Locale
 import java.util.Locale;
 import java.util.ArrayList;
 import java.io.FileReader; 
 import java.util.Scanner;
 
 
-
 class BackpackSolver{
-
     int number_of_objects;
     String[] names;
     int[] weights;
-    ArrayList<int> solution;
+    ArrayList<Integer> solution;
 
     public void BackpackSolver( int[] weights, String[] names, int size ){
         this.names = names;
@@ -26,7 +22,7 @@ class BackpackSolver{
     }
 
     public void fitObjects(){}
-
+/*
     public static void printSolution(){
         for(){
             System.out.printf( "Suitcase: %d\n" );
@@ -36,31 +32,30 @@ class BackpackSolver{
             System.out.println();
         }
     }
-
+    */
 }
 
 
 class Algorithm4{
 
     public static void main( String args[] ) throws Exception {
-        
         Locale.setDefault(Locale.US);
         int number_of_objects;
-        Scanner scam = new Scanner( args[0] );
+        FileReader reader = new FileReader( args[0] );
+        Scanner scam = new Scanner( reader );
         number_of_objects = scam.nextInt();
+        scam.nextLine();
         String[] object_names = new String[number_of_objects];
         int[] object_weights = new int[number_of_objects];
         String input_value;
 
         for( int i=0; i<number_of_objects; i++ ){
-            input_value = scam.nextLine():
-            String[] line = input_value.split(" ");
+            String[] line = scam.nextLine().trim().replaceAll(" +", " ").split(" ");
             object_names[i] = line[0];
             object_weights[i] = Integer.parseInt(line[1]);
             System.out.printf( "%s,%d\n", object_names[i], object_weights[i] );
         }
         
         BackpackSolver solver = new BackpackSolver( object_weights, object_names, number_of_objects );
-
     }
 }
